f34da9a8e872d2880a2f5edd17b02a2a16c45b84
Julio Mota <87203157+Storckmota@users.noreply.github.com>
2023-05-22 17:26:10 -0300

    Adicionando externsão no google

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "Escreva explicações e tutoriais visando ensinar conceitos blockchain para pessoas que nunca tiveram contato com a tecnologia mencionada";
+const generateAction = async (req, res) => {
+  // Execute o primeiro prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 1,
+    max_tokens: 1634,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
